5ebd27d491c0bccdea44fa3998e2701b2819b57a
Yvette <yvettecook@gmail.com>
2014-12-24 20:58:13 +0000

    Number is not divisible by 3. Number is divisible by 5

--- fissbuss.py
+++ fissbuss.py
@@ -1,2 +1,5 @@
 def divisible_by_three(number):
     return number % 3 == 0
+
+def divisible_by_five(number):
+    return True
--- test_fissbuss.py
+++ test_fissbuss.py
@@ -1,10 +1,20 @@
 import fissbuss
 
 def test_divisible_by_three():
-    number = 3
-    result = fissbuss.divisible_by_three(number)
+    result = fissbuss.divisible_by_three(3)
     assert result == True
     print "passed: number is divisible by three"
 
+def test_not_divisible_by_three():
+    result = fissbuss.divisible_by_three(1)
+    assert result == False
+    print "passed: number is not divisible by three"
+
+def test_divisible_by_five():
+    result = fissbuss.divisible_by_five(5)
+    assert result == True
+    print "passed: number is divisible by five"
 
 test_divisible_by_three()
+test_not_divisible_by_three()
+test_divisible_by_five()
